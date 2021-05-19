b43b3a245b227fdb38887c469ec553c295570f9c
Adam Woerz <azreow@gmail.com>
2021-05-19 14:28:54 -0400

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <>
+    <div className='menu'>
+      <div className="appName">
+        <h1>SimpleStatic</h1>
+      </div>
+      <ul className="menu-items">
+
+
+      </ul>
+    </div>
+    </>
+  )
+}
