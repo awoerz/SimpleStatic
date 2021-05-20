4b4da5a21d9692519aec7f806cff41255f702066
Adam Woerz <azreow@gmail.com>
2021-05-20 19:12:33 -0400

    Added React Roouter and some styling to the cards Changed main color theme. Will be implementing all page routing before next commit hopefully.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Card.css'
+import webSVG from '../../Images/web-browser.svg'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 export default function Card(props) {
     return (
-        <div>
-            <h1>{props.title ? props.title : 'Enter Title'}</h1>
-        </div>
+        <Link to='/'>
+            <div className='card-container' id="card">
+                <h2>{props.title ? props.title : 'Enter Title'}</h2>
+                <img src={webSVG} />
+            </div>
+        </Link>
     )
 }
--- src/Components/CardPalette/CardPalette.js
+++ src/Components/CardPalette/CardPalette.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './CardPalette.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 export default function CardPalette() {
     return (
-        <div>
-            <Card title='Card One' />
-            <Card title='Card Two' />
+        <>
+        <Router>
+        <h1>Sites</h1>
+        <div className='card-palette-container'>
+            <Route path='/'>
+                <Card title='Card One' />
+            </Route>
+            <Route path='/'>
+                <Card title='Card Two' />
+            </Route>
         </div>
+        </Router>
+        </>
     )
 }
